a32b8073d9ab7da656a318023dd9f6341b8e844c
Arish Islam <arishislam096@gmail.com>
2026-08-07 09:26:02 +0530

    Add files via upload

---
+++ Nested Loops/example.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+    int n = 10;
+    for(int i = 1; i <= n; i++){
+       int x = 10;
+       for(int j = 1; j <=x; j++){
+           cout << "*" ;
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
